45bf00eea5564d79403e7a2f74cf3db96a56c4a2
Cale Suarez <calesuarez1201@gmail.com>
2026-08-26 15:02:03 +0800

    Added: Dynamic Username & Role

--- frontend/src/components/NavigationBar/NavigationBar.jsx
+++ frontend/src/components/NavigationBar/NavigationBar.jsx
@@ -3,24 +3,29 @@ import { ChevronDown } from 'lucide-react';
 import { useNavigate } from "react-router-dom";
 import { useState } from 'react';
 
-function NavigationBar() {
+function NavigationBar({ children, role }) {
     const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState(false);
 
+    function logoutProcess() {
+        localStorage.removeItem('username');
+        localStorage.removeItem('role');
+        navigate('/');
+    }
     return(
         <div className="navbar-container">
             <nav className='navbar'>
                 <h3 onClick = {() => navigate("/dashboard_end")} className='page-logo'>A.T.L.A.S</h3>
-                <h3 className='greeting'>welcome gelo_o</h3>
+                <h3 className='greeting'>{children}</h3>
 
                 <div className="profile-dropdown">
                     <button className="profile" onClick = {() => setIsOpen(!isOpen)}>
                         <span className='profile-picture'>CS</span>
-                        <h3 className='user-type'>End User</h3>
+                        <h3 className='user-type'>{role}</h3>
                         <ChevronDown className={`dropdown ${isOpen ? "rotated" : ""}`}/>
                     </button>
 
-                    <button className={`dropdown-option ${isOpen ? "show" : ""}`} onClick = {() => navigate('/')}>Logout</button>
+                    <button className={`dropdown-option ${isOpen ? "show" : ""}`} onClick = {logoutProcess}>Logout</button>
 
                 </div>
             </nav>
