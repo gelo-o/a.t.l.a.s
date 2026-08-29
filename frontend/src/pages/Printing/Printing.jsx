bb8e0f1ba987250662d38700d4ca2a0b207d7866
Cale Suarez <calesuarez1201@gmail.com>
2026-08-29 14:08:38 +0800

    Fixed: Main Size | Added: Print Setting

--- frontend/src/pages/Printing/Printing.jsx
+++ frontend/src/pages/Printing/Printing.jsx
@@ -9,27 +9,44 @@ function Printing() {
             <NavigationBar role={role}>Printing Service</NavigationBar>
 
             <main>
-                <div className="drop-container">
+                <section className="drop-container">
                     <span className='drop-logo'>
                         <FileDown color='black' strokeWidth={1}
                         size={100}/>
                     </span>
-                    <h2 className='dropfile-title'>Drop File</h2>
-                    <h3 className='files-acceptable'>PDF Files Only</h3>
+                    <h1 className='dropfile-title'>Drop File</h1>
+                    <h2 className='files-acceptable'>PDF Files Only</h2>
                     <input type="file" />
-                </div>
+                </section>
 
-                <div className="right-side">
-                    
-                    <div className="print-setting">
-                        <h1>This is the printing setting</h1>
-                    </div>
+                <section className='print-container'>
+                    <h1 className='print-title'>Print Setting</h1>
+                    <hr className='divider'/>
 
-                    <div className="queue">
-                        <h1>This is the queue area</h1>
-                    </div>
+                    <form className='print-form'>
+                        <label className='print-label' htmlFor='printer'>Printer</label>
+                        <select name="printer" id="printer" className='printer-option'>
+                            <option value="office-a">Office Printer A</option>
+                            <option value="home-office">Home Office Jet</option>
+                        </select>
+                        <label className='print-label' htmlFor="paper-size">Paper Size</label>
+                        <select name="paper-size" id="paper-size" className='printer-option'>
+                            <option value="a4">A4</option>
+                            <option value="short">short</option>
+                            <option value="long">long</option>
+                        </select>
+                        <label className='print-label' htmlFor="paper-pages">Pages</label>
+                        <select name="paper-pages" id="paper-pages" className='printer-option-2'>
+                            <option value="all">All</option>
+                            <option value="odd">Odd Pages Only</option>
+                            <option value="even">Even Pages Only</option>
+                            <option value="custom">Custom</option>
+                        </select>
 
-                </div>
+
+
+                    </form>
+                </section>
             </main>
         </div>
     );
